ec92bf00ee96279eb8aff3dd53d6632e46db4c73
Nuh0804 <nuh.saidi.buhero@gmail.com>
2026-09-15 16:51:07 +0200

    Enhance PageShell component to scroll to top on route change using useLayoutEffect and useLocation from react-router, improving user navigation experience.

--- src/components/layout/PageShell.tsx
+++ src/components/layout/PageShell.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router";
+import { useLayoutEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 import { SiteFooter } from "@/components/layout/SiteFooter";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { ThemeProvider } from "@/hooks/ThemeProvider";
 
 export function PageShell() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
+
   return (
     <ThemeProvider>
       <div className="flex min-h-svh flex-col bg-background">
